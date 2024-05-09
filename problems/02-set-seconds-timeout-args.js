70e1093584dc600f37eb34d04ddb61f9fd4442d3
Jeffrey Manning <jeffmgpfit@yahoo.com>
2024-05-09 11:52:51 -0400

    02 done

--- problems/02-set-seconds-timeout-args.js
+++ problems/02-set-seconds-timeout-args.js
@@ -23,7 +23,9 @@ setSecondsTimeoutArgs(function(arg1, arg2) {
 ***********************************************************************/
 
 function setSecondsTimeoutArgs(cb, delayInSeconds, ...args) {
-  // Your code here
+  const res = cb(...args);
+
+  return setTimeout (res, delayInSeconds * 1000);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
